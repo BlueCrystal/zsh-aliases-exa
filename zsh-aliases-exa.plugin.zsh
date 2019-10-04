5d8857b6a58a3402c4ec48a47dd38ebe31fb0acc
Alex Eyre <38869148+alex-eyre@users.noreply.github.com>
2019-10-04 08:58:03 +0100

    WSL ignore support

--- zsh-aliases-exa.plugin.zsh
+++ zsh-aliases-exa.plugin.zsh
@@ -10,12 +10,13 @@
 
 # general use
 alias ls='exa'                                                         # ls
-alias l='exa -lbF --git'                                               # list, size, type, git
-alias ll='exa -lbGF --git'                                             # long list
-alias llm='exa -lbGF --git --sort=modified'                            # long list, modified date sort
-alias la='exa -lbhHigUmuSa --time-style=long-iso --git --color-scale'  # all list
+alias exa_wsl="exa -I NTUSER.DAT\*\|ntuser.\*"
+alias l='exa_wsl --group-directories-first -lbF --git'                                               # list, size, type, git
+alias ll='exa_wsl -lbGF --git'                                             # long list
+alias llm='exa_wsl -lbGF --git --sort=modified'                            # long list, modified date sort
+alias la='exa_wsl -lbhHigUmuSa --time-style=long-iso --git --color-scale'  # all list
 alias lx='exa -lbhHigUmuSa@ --time-style=long-iso --git --color-scale' # all + extended list
 
 # speciality views
-alias lS='exa -1'			                                                  # one column, just names
-alias lt='exa --tree --level=2'                                         # tree
+alias lS='exa_wsl -1'			                                                  # one column, just names
+alias lt='exa_wsl --tree --level=2'                                         # tree
